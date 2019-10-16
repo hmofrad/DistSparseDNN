95fce823e57a2154a665e83fac1b055ba454d6f3
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-10-16 00:17:37 -0400

    Update src/net.hpp

---
+++ src/net.hpp
@@ -0,0 +1,19 @@
+/*
+ * net.hpp: Neural network base class 
+ * (c) Mohammad Hasanzadeh Mofrad, 2019
+ * (e) m.hasanzadeh.mofrad@gmail.com 
+ */
+ 
+#ifndef NET_HPP
+#define NET_HPP
+
+template<typename Weight>
+class Net {
+    public:
+        Net() {};
+        ~Net() {};
+        
+};
+
+
+#endif 
