ebfb600e8ddeeb85f95f002afc07557eeb8e76e9
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-10-16 00:17:13 -0400

    Update src/log.hpp

--- src/log.hpp
+++ src/log.hpp
@@ -12,15 +12,15 @@
 namespace Logging {
     bool enabled = false;
     bool print_at_rank_zero = true;
-    enum LOGLEVELS {TRACE, DEBUG, INFO, WARN, ERROR, FATAL};
-    const char* LEVELS[] = {"TRACE", "DEBUG", "INFO", "WARN", "ERROR", "FATAL"};
+    enum LOG_LEVEL {TRACE, DEBUG, INFO, WARN, ERROR, FATAL};
+    const char* LOG_LEVELS[] = {"TRACE", "DEBUG", "INFO", "WARN", "ERROR", "FATAL"};
     void print(int log_level, const char* format, ...);
 }
 
 void Logging::print(int log_level, const char* format, ...) {
     if(enabled) {
         if((print_at_rank_zero and !Env::rank) or (not print_at_rank_zero)) {
-            printf("%s[rank=%d]: ", Logging::LEVELS[log_level], Env::rank);
+            printf("%s[rank=%d]: ", Logging::LOG_LEVELS[log_level], Env::rank);
             va_list arglist;  
             va_start(arglist, format);
             vprintf(format, arglist);
