249e084416552fb24ad036a929e064653b3adbdd
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-10-20 22:31:05 -0400

    Update src/log.hpp

--- src/log.hpp
+++ src/log.hpp
@@ -21,7 +21,7 @@ void Logging::print(int log_level, const char* format, ...) {
     if(enabled) {
         if((print_at_rank_zero and !Env::rank) or (not print_at_rank_zero)) {
             if(Logging::LOG_LEVELS[log_level] != LOG_LEVELS[VOID]) {
-                printf("%s[rank=%d]: ", Logging::LOG_LEVELS[log_level], Env::rank);
+                printf("%s[rank=%d] ", Logging::LOG_LEVELS[log_level], Env::rank);
             }
             va_list arglist;  
             va_start(arglist, format);
