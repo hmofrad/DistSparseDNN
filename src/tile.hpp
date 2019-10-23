12607b0956789656d480c4e6ee0bce6082daaa1f
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-10-23 00:23:18 -0400

    Update src/tile.hpp

--- src/tile.hpp
+++ src/tile.hpp
@@ -13,26 +13,12 @@
 template<typename Weight>
 struct Tile{
     public:
-        Tile() {};//printf("CREATING Tile = %d %p\n", spmat != nullptr, spmat);};
-        ~Tile() {
-       //     printf("1.~tile: Delete\n"); 
-     //       std::cout << spmat.use_count() << std::endl;
-   //spmat.reset();
-   //std::cout << spmat.use_count() << std::endl;
-            /*
-            if(spmat) {
-                delete spmat;
-                printf("2.~tile: Delete %d %p\n", spmat != nullptr, spmat); 
-                spmat = nullptr;
-            }
-            */
-            //printf("3.~tile: Delete %d %p\n", spmat != nullptr, spmat); 
-        };
+        Tile() {};
+        ~Tile() {};
         
         std::vector<struct Triple<Weight>> triples;
-        ////struct Compressed_Block<Weight>* spmat = nullptr;
-        //std::unique_ptr<struct Compressed_Block<Weight>> spmat1;
-        std::shared_ptr<struct Compressed_Block<Weight>> spmat = nullptr;
+        std::shared_ptr<struct Compressed_Format<Weight>> spmat = nullptr;
+        //std::unique_ptr<struct Compressed_Format<Weight>> spmat();// = nullptr;
         
         int32_t rank;
         uint64_t nedges;
