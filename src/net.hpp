6e4a67be42b5dca51731ac0194b90ee885c2d1be
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-10-24 23:43:49 -0400

    Update src/net.hpp

--- src/net.hpp
+++ src/net.hpp
@@ -10,6 +10,7 @@
 #include "triple.hpp"
 #include "tiling.hpp"
 #include "dvec.hpp"
+#include "spops.hpp"
 
 template<typename Weight>
 class Net {
@@ -26,8 +27,8 @@ class Net {
         std::unique_ptr<struct Tiling<Weight>> tiling = nullptr;
         std::vector<uint32_t> trueCategories;
         std::vector<std::unique_ptr<struct Tiling<Weight>>> layers;
-        //std::vector<std::unique_ptr<struct DVec<Weight>>> biasDenseVecs;
         std::vector<std::vector<Weight>> biasDenseVecs;
+        std::vector<std::vector<Weight>> spaDenseVec;
         
         uint32_t Nneurons;
         Weight biasValue;
@@ -39,7 +40,8 @@ Net<Weight>::Net(const uint32_t Nneurons_, const std::string inputFile_prefix,
                  const uint32_t maxLayers_, const std::string layerFile_prefix,
                  const INPUT_TYPE input_type, const TILING_TYPE tiling_type, 
                  const COMPRESSED_FORMAT compression_type) : Nneurons(Nneurons_), maxLayers(maxLayers_) {
-                     
+    
+    Logging::print(Logging::LOG_LEVEL::INFO, "Neural network: Processing input feature file for %d neurons.\n", Nneurons);  
     std::vector<Weight> neuralNetBias = {-0.3,-0.35,-0.4,-0.45};
     std::vector<uint32_t> NneuronsVector = {1024, 4096, 16384, 65536};    
     uint32_t idxN = std::distance(NneuronsVector.begin(), std::find(NneuronsVector.begin(), NneuronsVector.end(), Nneurons));
@@ -55,7 +57,7 @@ Net<Weight>::Net(const uint32_t Nneurons_, const std::string inputFile_prefix,
     tiling = std::move(std::make_unique<Tiling<Weight>>((Env::nranks * Env::nranks), Env::nranks, Env::nranks, Env::nranks,
                         feature_file, input_type, tiling_type, compression_type));
     
-    
+    Logging::print(Logging::LOG_LEVEL::INFO, "Neural network: Processing the category files for %d neurons and %d layers.\n", Nneurons, maxLayers); 
     std::vector<uint32_t> maxLayersVector = {120, 480, 1920};
     uint32_t idxL = std::distance(maxLayersVector.begin(), std::find(maxLayersVector.begin(), maxLayersVector.end(), maxLayers));
     if(idxL >= maxLayersVector.size()) {
@@ -73,63 +75,40 @@ Net<Weight>::Net(const uint32_t Nneurons_, const std::string inputFile_prefix,
     else {
         IO::binary_file_categories(categoryFile, trueCategories, tiling->tile_height);
     }
-    
-    /*
-    int k = 0;
-   // if(Env::rank == 3) {
-        for(auto& c: trueCategories) {
-            if(c == 1) {
-                k++;  
-               // printf("%d %d %d\n", c, c + (Env::rank * tiling->tile_height), k);
-            }
-        }
-    //}
-    */
-    //printf("2. %d %d\n", Env::rank, k);
-    
 
-
+    Logging::print(Logging::LOG_LEVEL::INFO, "Neural network: Processing %d layer files (silent).\n", maxLayers); 
     Logging::enabled = false; 
     layers.resize(maxLayers);
     biasDenseVecs.resize(maxLayers);
     for(uint32_t i = 0; i < maxLayers; i++) {
         std::string layerFile = layerFile_prefix + "/neuron" + std::to_string(Nneurons) + "/n" + std::to_string(Nneurons) + "-l" + std::to_string(i+1);
         layerFile += (input_type == INPUT_TYPE::_TEXT_) ? ".tsv" : ".bin";
-        
-        //layers.push_back(std::move(std::make_unique<Tiling<Weight>>((Env::nranks * Env::nranks), Env::nranks, Env::nranks, Env::nranks,
-          //               layerFile, input_type, tiling_type, compression_type)));
         layers[i] = std::move(std::make_unique<Tiling<Weight>>((Env::nranks * Env::nranks), Env::nranks, Env::nranks, Env::nranks,
                          layerFile, input_type, tiling_type, compression_type)); 
-        
-        //for(int32_t j = 0; j < Env::nranks; j++) {
-        //biasDenseVecs.push_back(std::move(std::make_unique<struct DVec<Weight>>(layers[i]->tile_height, biasValue)));
-        //biasDenseVecs.push_back(std
         biasDenseVecs[i] = std::vector<Weight>(tiling->tile_height, biasValue);
-        
-       //if(!Env::rank) printf("%s %lu\n", layerFile.c_str(), biasDenseVecs[i]->nitems);
-       
-    //   break;
     }
+    spaDenseVec.resize(tiling->tile_height);
     Logging::enabled = true;
+    Logging::print(Logging::LOG_LEVEL::INFO, "Neural network: Running the inferenceReLU method.\n"); 
+    uint32_t nrows = 0;
+    uint32_t ncols = 0;
+    uint64_t nnz = 0;
     
+    //std::tie(nrows, ncols, nnz) =  spmm_sym();
     
-
-
+    /*    
+    start = std::chrono::high_resolution_clock::now();
+    inferenceReLU<WGT>(layersSpMat, biasesDenseVec, featuresSpMat, spa_VEC); // Train DNN 
+    finish = std::chrono::high_resolution_clock::now();
+    WGT challengeRunTime = (WGT)(std::chrono::duration_cast< std::chrono::nanoseconds>(finish-start).count())/1e9;
+    WGT challengeRunRate = NfeatureVectors * (DNNedges/challengeRunTime);
+    printf("INFO: Run time (sec): %f, run rate (edges/sec): %f\n", challengeRunTime, challengeRunRate);
     
-    
-    //printf("INFO: Start reading %d layer files\n", maxLayers);
-    //auto start = std::chrono::high_resolution_clock::now();
-    //for(uint32_t i = 0; i < maxLayers; i++) {  
-      //  std::string layerFile = ((std::string) argv[6]) + "/neuron" + std::to_string(Nneurons) + "/n" + std::to_string(Nneurons) + "-l" + std::to_string(i+1) + ".tsv";
-    //}
-      //           }
-    
-    /*
-    std::string categoryFile = ((std::string) argv[6]) + "/neuron" + std::to_string(Nneurons) + "-l" + std::to_string(maxLayers) + "-categories.tsv";
-    printf("INFO: Start reading the category file %s\n", categoryFile.c_str());
+    validate_prediction<WGT>(featuresSpMat, trueCategories); // Test DNN
     */
     
     
+    Logging::print(Logging::LOG_LEVEL::INFO, "Neural network: inferenceReLU? %d %d %lu\n", nrows, ncols, nnz); 
 }
 
 #endif 
