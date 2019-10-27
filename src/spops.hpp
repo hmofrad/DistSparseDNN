638c43274b1d0506275d218c4bed4c68888e41eb
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-10-27 16:55:30 -0400

    Update src/spops.hpp

---
+++ src/spops.hpp
@@ -0,0 +1,242 @@
+/*
+ * spops.cpp: Sparse Matrix operations implementation
+ * Sparse Matrix - Sparse Matrix (SpMM)
+ * (c) Mohammad Hasanzadeh Mofrad, 2019
+ * (e) m.hasanzadeh.mofrad@gmail.com
+ */
+ 
+#ifndef SPOPS_H
+#define SPOPS_H
+
+#include "env.hpp"
+#include "spmat.hpp"
+
+template<typename Weight>
+//inline void spmm_sym();
+//inline void spmm();
+
+ 
+
+inline std::tuple<uint32_t, uint32_t, uint64_t> spmm_sym(std::shared_ptr<struct Compressed_Format<Weight>> A,
+                                                         std::shared_ptr<struct Compressed_Format<Weight>> B,
+                                                         std::vector<Weight> s) {
+
+    uint64_t nnzmax = 0;
+    uint32_t nrows = 0;
+    uint32_t ncols = 0;
+    if((A->compression_type == COMPRESSED_FORMAT::_CSR_) and (B->compression_type == COMPRESSED_FORMAT::_CSR_)) {
+        const std::shared_ptr<struct CSR<Weight>> A_CSR = std::static_pointer_cast<struct CSR<Weight>>(A);
+        const uint64_t A_nnz   = A_CSR->nnz;
+        const uint32_t A_nrows = A_CSR->nrows;
+        const uint32_t A_ncols = A_CSR->ncols;
+        const uint32_t* A_IA   = A_CSR->IA_blk->ptr;
+        const uint32_t* A_JA   = A_CSR->JA_blk->ptr;
+        const Weight*    A_A   = A_CSR->A_blk->ptr;
+        
+        const std::shared_ptr<struct CSR<Weight>> B_CSR = std::static_pointer_cast<struct CSR<Weight>>(B);
+        const uint64_t B_nnz   = B_CSR->nnz;
+        const uint32_t B_nrows = B_CSR->nrows;
+        const uint32_t B_ncols = B_CSR->ncols;
+        const uint32_t* B_IA   = B_CSR->IA_blk->ptr;
+        const uint32_t* B_JA   = B_CSR->JA_blk->ptr;
+        const Weight*    B_A   = B_CSR->A_blk->ptr;
+        
+        if(A_ncols != B_nrows) {
+            Logging::print(Logging::LOG_LEVEL::ERROR, "SpMM dimensions do not agree A[%d %d] B[%d %d]\n", A_nrows, A_ncols, B_nrows, B_ncols);
+            std::exit(Env::finalize()); 
+        }
+        
+        //printf("CSR: %d %d %lu %d %d\n", A->compression_type, B->compression_type, nnz, nrows, ncols);
+    }
+    else if((A->compression_type == COMPRESSED_FORMAT::_CSC_) and (B->compression_type == COMPRESSED_FORMAT::_CSC_)) {
+        const std::shared_ptr<struct CSC<Weight>> A_CSC = std::static_pointer_cast<struct CSC<Weight>>(A);
+        const uint64_t A_nnz   = A_CSC->nnz;
+        const uint32_t A_nrows = A_CSC->nrows;
+        const uint32_t A_ncols = A_CSC->ncols;
+        const uint32_t* A_IA   = A_CSC->IA_blk->ptr;
+        const uint32_t* A_JA   = A_CSC->JA_blk->ptr;
+        const Weight*    A_A   = A_CSC->A_blk->ptr;
+        
+        const std::shared_ptr<struct CSC<Weight>> B_CSC = std::static_pointer_cast<struct CSC<Weight>>(B);
+        const uint64_t B_nnz   = B_CSC->nnz;
+        const uint32_t B_nrows = B_CSC->nrows;
+        const uint32_t B_ncols = B_CSC->ncols;
+        const uint32_t* B_IA   = B_CSC->IA_blk->ptr;
+        const uint32_t* B_JA   = B_CSC->JA_blk->ptr;
+        const Weight*    B_A   = B_CSC->A_blk->ptr;
+        
+        if(A_ncols != B_nrows) {
+            Logging::print(Logging::LOG_LEVEL::ERROR, "SpMM dimensions do not agree A[%d %d] B[%d %d]\n", A_nrows, A_ncols, B_nrows, B_ncols);
+            std::exit(Env::finalize()); 
+        }
+        Logging::print(Logging::LOG_LEVEL::ERROR, "SpMM dimensions do not agree A[%d %d] B[%d %d] %lu\n", A_nrows, A_ncols, B_nrows, B_ncols, s.size());
+        /*
+        for(uint32_t j = 0; j < B_ncols; j++) {
+            for(uint32_t k = B_JA[j]; k < B_JA[j+1]; k++) {
+                uint32_t l = B_IA[k];
+                for(uint32_t m = A_JA[l]; m < A_JA[l+1]; m++) {
+                    s[A_IA[m]] = 1;
+                }
+            }
+            for(uint32_t i = 0; i < A_nrows; i++) {
+                if(s[i]) {
+                    nnzmax++;
+                    s[i] = 0;
+                }
+            }
+        }
+        
+        */
+        
+        
+        printf("CSC: %d %d %lu %d %d\n", A->compression_type, B->compression_type, nnzmax, nrows, ncols);
+    }
+    
+    
+    
+    /*
+    if((compression_type == COMPRESSED_FORMAT::_CSR_)  or
+    spmat = std::make_shared<CSR<Weight>>(triples.size(), tile_height, tile_width);
+    const uint64_t nnz = static_cast<CSR<Weight>(tile.compressor)->nnz;
+    
+    uint32_t* IA = CSC::IA_blk->ptr;
+    uint32_t* JA = CSC::JA_blk->ptr;
+    Weight* A = CSC::A_blk->ptr;
+    
+    uint64_t nnzmax_local = 0;
+    
+    for(uint32_t j = start; j < end; j++) {
+        for(uint32_t k = B_JA[j]; k < B_JA[j+1]; k++) {
+            uint32_t l = B_IA[k];
+            for(uint32_t m = A_JA[l]; m < A_JA[l+1]; m++) {
+                s_A[A_IA[m]] = 1;
+            }
+        }
+        for(uint32_t i = 0; i < A_nrows; i++) {
+            if(s_A[i]) {
+                nnzmax_local++;
+                s_A[i] = 0;
+            }
+        }
+    }
+    */
+  
+    return std::make_tuple(nrows + 1, ncols + 1, nnzmax);
+}
+
+inline void spmm() {
+    
+}
+
+#endif
+
+/*
+template<typename Weight>
+inline void SpMM_Sym(struct CSC<Weight> *A_CSC, struct CSC<Weight> *B_CSC, struct CSC<Weight> *C_CSC, 
+                     struct DenseVec<Weight> *s, int tid) { 
+    uint32_t *A_JA = A_CSC->JA;
+    uint32_t *A_IA = A_CSC->IA;      
+    Weight   *A_A  = A_CSC->A;
+    uint32_t A_nrows = A_CSC->nrows;  
+    uint32_t A_ncols = A_CSC->ncols;
+    
+    uint32_t *B_JA = B_CSC->JA;
+    uint32_t *B_IA = B_CSC->IA;      
+    Weight   *B_A  = B_CSC->A;
+    uint32_t B_nrows = B_CSC->nrows;  
+    uint32_t B_ncols = B_CSC->ncols;
+    
+    auto *s_A = s->A;
+    
+    uint64_t nnzmax = 0;        
+    if(A_ncols != B_nrows) {
+        fprintf(stderr, "Error: SpMM dimensions do not agree A[%d %d] B[%d %d]\n", A_nrows, A_ncols, B_nrows, B_ncols);
+        exit(1);
+    }
+
+    Env::env_unset(tid);
+    int nthreads = omp_get_num_threads();
+    uint32_t length = B_ncols;
+    uint32_t chunk = length/nthreads;
+    uint32_t start = chunk * tid;
+    uint32_t end = start + chunk;
+    end = (tid == nthreads - 1) ? length : end;
+    uint64_t nnzmax_local = 0;
+    
+    for(uint32_t j = start; j < end; j++) {
+        for(uint32_t k = B_JA[j]; k < B_JA[j+1]; k++) {
+            uint32_t l = B_IA[k];
+            for(uint32_t m = A_JA[l]; m < A_JA[l+1]; m++) {
+                s_A[A_IA[m]] = 1;
+            }
+        }
+        for(uint32_t i = 0; i < A_nrows; i++) {
+            if(s_A[i]) {
+                nnzmax_local++;
+                s_A[i] = 0;
+            }
+        }
+    }
+    Env::start_col[tid] = start;
+    Env::end_col[tid] = end;
+    Env::length_nnz[tid] = nnzmax_local;
+        
+    #pragma omp barrier
+    if(!tid) {
+        nnzmax = Env::env_set();
+        uint32_t nrows = A_CSC->nrows;
+        uint32_t ncols = B_CSC->ncols;        
+        C_CSC->initialize(nrows, ncols, nnzmax);
+    }
+    #pragma omp barrier
+}
+
+template<typename Weight>
+inline void SpMM(struct CSC<Weight> *A_CSC, struct CSC<Weight> *B_CSC, struct CSC<Weight> *C_CSC,
+                  struct DenseVec<Weight> *s, struct DenseVec<Weight> *b, int tid) {  
+    uint32_t *A_JA = A_CSC->JA;
+    uint32_t *A_IA = A_CSC->IA;      
+    Weight   *A_A  = A_CSC->A;
+    uint32_t A_nrows = A_CSC->nrows;  
+    uint32_t A_ncols = A_CSC->ncols;    
+    
+    uint32_t *B_JA = B_CSC->JA;
+    uint32_t *B_IA = B_CSC->IA;      
+    Weight   *B_A  = B_CSC->A;
+    uint32_t B_nrows = B_CSC->nrows;
+    uint32_t B_ncols = B_CSC->ncols;
+
+    uint32_t C_nrows = C_CSC->nrows;
+    uint32_t C_ncols = C_CSC->ncols;;       
+                 
+    uint32_t b_nitems = b->nitems;
+    
+    if((A_ncols != B_nrows) or (A_nrows != C_nrows) or (B_ncols != C_ncols)) {
+        fprintf(stderr, "Error: SpMM dimensions do not agree C[%d %d] != A[%d %d] B[%d %d]\n", C_nrows, C_ncols, A_nrows, A_ncols, B_nrows, B_ncols);
+        exit(1);
+    }
+    
+    if(C_ncols != b_nitems) {
+        fprintf(stderr, "Error: SpMV_EW dimensions do not agree [%d != %d]\n", C_ncols, b_nitems);
+        exit(1);
+    }
+
+    uint32_t start = Env::start_col[tid];
+    uint32_t end = Env::end_col[tid];
+    auto *s_A = s->A;
+
+    for(uint32_t j = start; j < end; j++) {
+        for(uint32_t k = B_JA[j]; k < B_JA[j+1]; k++) {
+            uint32_t l = B_IA[k];
+            for(uint32_t m = A_JA[l]; m < A_JA[l+1]; m++) {
+                s_A[A_IA[m]] += B_A[k] * A_A[m];
+            }
+        }
+        C_CSC->spapopulate_t(b, s, j, tid);
+    }
+    #pragma omp barrier
+    C_CSC->postpopulate_t(tid);
+    A_CSC->repopulate(C_CSC, tid);
+    #pragma omp barrier
+}
+*/
