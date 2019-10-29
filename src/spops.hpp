f90f0febce1af2e3e9a050b779bf5f65c5a18007
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-10-29 18:39:18 -0400

    Update src/spops.hpp

--- src/spops.hpp
+++ src/spops.hpp
@@ -17,7 +17,7 @@
 
  
 template<typename Weight>
-inline std::tuple<uint32_t, uint32_t, uint64_t> spmm_sym(std::shared_ptr<struct Compressed_Format<Weight>> A,
+inline std::tuple<uint64_t, uint32_t, uint32_t> spmm_sym(std::shared_ptr<struct Compressed_Format<Weight>> A,
                                                          std::shared_ptr<struct Compressed_Format<Weight>> B,
                                                          std::vector<Weight> s) {
 
@@ -75,9 +75,9 @@ inline std::tuple<uint32_t, uint32_t, uint64_t> spmm_sym(std::shared_ptr<struct 
         }
         nrows = A_nrows;
         ncols = B_ncols;
-       Logging::print(Logging::LOG_LEVEL::ERROR, "SpMM dimensions do not agree A[%d %d] B[%d %d]\n", A_nrows, A_ncols, B_nrows, B_ncols);
-       std::vector<bool> ss(A_nrows);
-       uint64_t ii = 0;
+       //Logging::print(Logging::LOG_LEVEL::ERROR, "SpMM dimensions do not agree A[%d %d] B[%d %d]\n", A_nrows, A_ncols, B_nrows, B_ncols);
+       //std::vector<bool> ss(A_nrows);
+       //uint64_t ii = 0;
        
        /*
         for(uint32_t j = 0; j < B_ncols; j++) {
@@ -96,12 +96,13 @@ inline std::tuple<uint32_t, uint32_t, uint64_t> spmm_sym(std::shared_ptr<struct 
             }
         }
         */
+        
         for(uint32_t j = 0; j < B_ncols; j++) {
-            //printf("%d %lu/%lu\n", j, C_CSC->nnz_i, C_CSC->nnz);
+            //printf("%d nnzmax=%lu\n", j, nnzmax);
             for(uint32_t k = B_JA[j]; k < B_JA[j+1]; k++) {
                 uint32_t l = B_IA[k];
                 for(uint32_t m = A_JA[l]; m < A_JA[l+1]; m++) {
-                    ii++;
+                    //ii++;
                     s[A_IA[m]] = 1;
                 }
             }
@@ -112,10 +113,13 @@ inline std::tuple<uint32_t, uint32_t, uint64_t> spmm_sym(std::shared_ptr<struct 
                 }
             }
         }
-        printf("1.ii=%lu %lu\n", ii, nnzmax);
+        
+        //A_CSC->walk(); 
+        //B_CSC->walk();
     }
-    
-    return std::make_tuple(nrows, ncols, nnzmax);
+ 
+    //printf("nnzmax=%lu\n", nnzmax);
+    return std::make_tuple(nnzmax, nrows, ncols);
 }
 
 template<typename Weight>
@@ -124,7 +128,7 @@ inline void spmm(std::shared_ptr<struct Compressed_Format<Weight>> A,
                  std::shared_ptr<struct Compressed_Format<Weight>> C,
                  std::vector<Weight> s,
                  std::vector<Weight> b) {
-                printf("SPMM %d\n", Env::rank);
+                //printf("SPMM %d\n", Env::rank);
                   
     if((A->compression_type == COMPRESSED_FORMAT::_CSC_) and 
        (B->compression_type == COMPRESSED_FORMAT::_CSC_) and
@@ -158,49 +162,36 @@ inline void spmm(std::shared_ptr<struct Compressed_Format<Weight>> A,
             Logging::print(Logging::LOG_LEVEL::ERROR, "SpMM dimensions do not agree C[%d %d] != A[%d %d] B[%d %d]\n", C_nrows, C_ncols, A_nrows, A_ncols, B_nrows, B_ncols);
             std::exit(Env::finalize()); 
         }
-        Logging::print(Logging::LOG_LEVEL::ERROR, "SpMM dimensions do not agree C[%d %d] != A[%d %d] B[%d %d]\n", C_nrows, C_ncols, A_nrows, A_ncols, B_nrows, B_ncols);
 
-        uint64_t ii = 0;
-        
-        uint64_t nnzmax = 0;
-                for(uint32_t j = 0; j < B_ncols; j++) {
-            //printf("%d %lu/%lu\n", j, C_CSC->nnz_i, C_CSC->nnz);
-            for(uint32_t k = B_JA[j]; k < B_JA[j+1]; k++) {
-                uint32_t l = B_IA[k];
-                for(uint32_t m = A_JA[l]; m < A_JA[l+1]; m++) {
-                    ii++;
-                    s[A_IA[m]] = 1;
-                }
-            }
-            for(uint32_t i = 0; i < A_nrows; i++) {
-                if(s[i]){
-                    nnzmax++;
-                    s[i] = 0;
-                }
-            }
-        }
-        printf("1.ii=%lu %lu\n", ii, nnzmax);
-        printf("XXXXXXXXXXXXXXXXXXXXXXXXXX\n");
+       // printf("1.ii=%lu %lu\n", ii, nnzmax);
+      //  printf("XXXXXXXXXXXXXXXXXXXXXXXXXX\n");
         
         
         
         
-        
+        //printf("0.nnz_i=%lu \n", C_CSC->nnz_i);
         for(uint32_t j = 0; j < B_ncols; j++) {
             //printf("%d %lu/%lu\n", j, C_CSC->nnz_i, C_CSC->nnz);
             for(uint32_t k = B_JA[j]; k < B_JA[j+1]; k++) {
                 uint32_t l = B_IA[k];
                 for(uint32_t m = A_JA[l]; m < A_JA[l+1]; m++) {
-                    //s[A_IA[m]] += (B_A[k] * A_A[m]);
-                    s[A_IA[m]] = 1;
-                    ii++;
+                    s[A_IA[m]] += (B_A[k] * A_A[m]);
+                    //s[A_IA[m]] = 1;
+                    //ii++;
                 }
             }
             C_CSC->populate_spa(s, b, j);
         }
-        printf("%lu %lu\n", C_CSC->nnz, C_CSC->nnz_i);
-        printf("ii=%lu\n", ii);
-    
+        //printf("1.nnz_i=%lu \n", C_CSC->nnz_i);
+        //printf("%lu %lu\n", C_CSC->nnz, C_CSC->nnz_i);
+        C_CSC->adjust();
+        //if(C_CSC->nnz != C_CSC->nnz_i) {
+            
+        //}
+        
+       //C_CSC->walk(); 
+     //A_CSC->repopulate(C_CSC);
+     
     /*
     if(C_ncols != b_nitems) {
         fprintf(stderr, "Error: SpMV_EW dimensions do not agree [%d != %d]\n", C_ncols, b_nitems);
@@ -208,7 +199,7 @@ inline void spmm(std::shared_ptr<struct Compressed_Format<Weight>> A,
     }
   */     
        
-       
+
        
        
    }
@@ -218,6 +209,66 @@ inline void spmm(std::shared_ptr<struct Compressed_Format<Weight>> A,
    }
     
 }
+
+
+template<typename Weight>
+inline void validate_prediction(std::shared_ptr<struct Compressed_Format<Weight>> A,
+                                      std::vector<uint32_t> trueCategories) {
+    const std::shared_ptr<struct CSC<Weight>> A_CSC = std::static_pointer_cast<struct CSC<Weight>>(A);
+    const uint64_t A_nnz   = A_CSC->nnz;
+    const uint32_t A_nrows = A_CSC->nrows;
+    const uint32_t A_ncols = A_CSC->ncols;
+    const uint32_t* A_IA   = A_CSC->IA_blk->ptr;
+    const uint32_t* A_JA   = A_CSC->JA_blk->ptr;
+    const Weight*    A_A   = A_CSC->A_blk->ptr;
+    
+    
+    
+    std::vector<uint32_t> allCategories(A_nrows);
+    for(uint32_t j = 0; j < A_ncols; j++) {
+        for(uint32_t i = A_JA[j]; i < A_JA[j+1]; i++) {
+            allCategories[A_IA[i]] = 1;
+        }
+    }
+    
+
+    
+//printf("%d %d %lu %lu\n", A_nrows, A_ncols, predictedCategories.size(), trueCategories.size());
+    bool tf = true;
+  //  if(trueCategories.size() == predictedCategories.size()) {        
+        uint32_t j = 0;
+        for(uint32_t i = 0; i < A_nrows; i++) {
+            //printf("%d %d %d\n", i, trueCategories[i], allCategories[i]);
+            if(trueCategories[i] != allCategories[i]) {
+                tf = false;
+                break;
+                //if(trueCategories[j] != (i - 1)) {
+                    //tf = false;
+                   // break;
+                ///}
+             //   j++;
+            }
+            //if(trueCategories[i] != allCategories[i]) {
+              //  tf = false;
+                //break;
+            //}
+        }
+    //} 
+    //else {
+      //  tf = false;
+    //}
+    
+    if(tf) {
+        printf("INFO: Challenge PASSED\n");
+    }
+    else {
+        printf("INFO: Challenge FAILED\n");
+    }
+    
+    
+}
+
+
 
 #endif
 /*
