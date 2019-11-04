fe1d7cb3701ff6ca2f0fafe90eec0a02309c6fc1
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-11-03 23:28:01 -0500

    Update src/tile.hpp

--- src/tile.hpp
+++ src/tile.hpp
@@ -21,13 +21,12 @@ struct Tile{
         
         std::vector<struct Triple<Weight>> triples;
         std::shared_ptr<struct Compressed_Format<Weight>> spmat = nullptr;
-        //std::unique_ptr<struct Compressed_Format<Weight>> spmat;
         
         int32_t rank;
         int32_t thread;
         uint64_t nedges = 0;
-        uint32_t start_col;
-        uint32_t end_col;
+        //uint32_t start_col;
+        //uint32_t end_col;
 };
 
 template<typename Weight>
@@ -52,6 +51,7 @@ void Tile<Weight>::sort(const COMPRESSED_FORMAT compression_type, const RowSort<
 template<typename Weight>
 void Tile<Weight>::compress(const COMPRESSED_FORMAT compression_type, uint64_t nnz, uint32_t tile_height, uint32_t tile_width) {  
     if(not triples.empty()) {
+        /*
         if(compression_type == COMPRESSED_FORMAT::_CSR_) {
             spmat = std::make_shared<CSR<Weight>>(triples.size(), tile_height, tile_width);
             spmat->populate(triples, tile_height, tile_width);
@@ -59,7 +59,6 @@ void Tile<Weight>::compress(const COMPRESSED_FORMAT compression_type, uint64_t n
             triples.clear();
             triples.shrink_to_fit();
         }
-        /*
         else if(compression_type == COMPRESSED_FORMAT::_DCSR_) {
             spmat = std::make_shared<DCSR<Weight>>();
         }
@@ -67,7 +66,8 @@ void Tile<Weight>::compress(const COMPRESSED_FORMAT compression_type, uint64_t n
             spmat = std::make_shared<TCSR<Weight>>();
         }
         */
-        else if(compression_type == COMPRESSED_FORMAT::_CSC_) {
+        //else 
+        if(compression_type == COMPRESSED_FORMAT::_CSC_) {
             spmat = std::make_shared<CSC<Weight>>(triples.size(), tile_height, tile_width);
             spmat->populate(triples, tile_height, tile_width);
             //spmat->walk();
@@ -84,11 +84,14 @@ void Tile<Weight>::compress(const COMPRESSED_FORMAT compression_type, uint64_t n
         */
     }
     else if(nnz) {
+        /*
         if(compression_type == COMPRESSED_FORMAT::_CSR_) {
             spmat = std::make_shared<CSR<Weight>>(nnz, tile_height, tile_width);
             //spmat->populate(tile_height, tile_width);
         }
-        else if(compression_type == COMPRESSED_FORMAT::_CSC_) {
+        else
+        */            
+        if(compression_type == COMPRESSED_FORMAT::_CSC_) {
             spmat = std::make_shared<CSC<Weight>>(nnz, tile_height, tile_width);
             //spmat->populate(tile_height, tile_width);
         } 
