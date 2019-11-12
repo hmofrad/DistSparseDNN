6189b926580e6990c69f8b1214135439688ad0e2
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-11-12 12:40:59 -0500

    Update src/spmat.hpp

--- src/spmat.hpp
+++ src/spmat.hpp
@@ -21,12 +21,18 @@ struct Compressed_Format {
     public:
         Compressed_Format() {};
         virtual ~Compressed_Format() {};
-        virtual void populate(std::vector<struct Triple<Weight>>& triples, uint32_t tile_height, uint32_t tile_width) {};
-        virtual void populate_spa(std::vector<Weight>& spa, std::vector<Weight> bias, const uint32_t& col, int32_t tid) {};
-        virtual void adjust(int32_t tid) {};
-        virtual void reallocate(uint64_t nnz_, uint32_t nrows_, uint32_t ncols_) {};
-        virtual void walk(int32_t tid) {};
-        virtual void statistics(int32_t tid) {};
+        virtual void populate(const std::vector<struct Triple<Weight>> triples, const uint32_t tile_height, const uint32_t tile_width) {};
+        //virtual void populate(const std::vector<struct Triple<Weight>>& triples, uint32_t nrows, uint32_t tile_height, uint32_t tile_width) {};
+        //virtual void populate(const std::vector<struct Triple<Weight>> triples, const uint32_t nrows, const uint32_t ncols,
+          //                    const uint32_t tile_height, const uint32_t tile_width) {};
+        virtual void refine_cols() {};
+        virtual void refine_rows(const uint32_t nrows) {};
+        virtual void populate_spa(std::vector<Weight>& spa, const std::vector<Weight> bias, const uint32_t col, const int32_t tid) {};
+        virtual void adjust(const int32_t tid) {};
+        virtual void reallocate(const uint64_t nnz_, const uint32_t nrows_, const uint32_t ncols_) {};
+        virtual void walk(const int32_t tid) {};
+        virtual void walk(const bool one_rank) {};
+        //virtual void statistics(const int32_t tid) {};
         
         COMPRESSED_FORMAT compression_type;
         
@@ -36,7 +42,7 @@ struct Compressed_Format {
         
         std::shared_ptr<struct Data_Block<uint32_t>> IA_blk;
         std::shared_ptr<struct Data_Block<uint32_t>> JA_blk;
-        std::shared_ptr<struct Data_Block<Weight>>   A_blk;
+        std::shared_ptr<struct Data_Block<Weight>>    A_blk;
 };
 
 template<typename Weight>
@@ -45,12 +51,18 @@ struct CSC: public Compressed_Format<Weight> {
         CSC(uint64_t nnz_, uint32_t nrows_, uint32_t ncols_);
         ~CSC(){};
         
-        void populate(std::vector<struct Triple<Weight>>& triples, uint32_t tile_height, uint32_t tile_width);
-        void populate_spa(std::vector<Weight>& spa, std::vector<Weight> bias, const uint32_t col, int32_t tid);
-        void walk(int32_t tid);
-        void reallocate(uint64_t nnz_, uint32_t nrows_, uint32_t ncols_);
-        void adjust(int32_t tid);
-        void statistics(int32_t tid);
+        void populate(const std::vector<struct Triple<Weight>> triples, const uint32_t tile_height, const uint32_t tile_width);
+        //void populate(std::vector<struct Triple<Weight>>& triples, uint32_t nrows, uint32_t tile_height, uint32_t tile_width) {};
+        //void populate(const std::vector<struct Triple<Weight>> triples, const uint32_t nrows, const uint32_t ncols, 
+        //              const uint32_t tile_height, const uint32_t tile_width) {};
+        void refine_cols();
+        void refine_rows(const uint32_t nrows);
+        void populate_spa(std::vector<Weight>& spa, const std::vector<Weight> bias, const uint32_t col, const int32_t tid);
+        void walk(const int32_t tid);
+        void walk(const bool one_rank);
+        void reallocate(const uint64_t nnz_, const uint32_t nrows_, const uint32_t ncols_);
+        void adjust(const int32_t tid);
+        //void statistics(const int32_t tid);
         
         uint64_t nnz_i = 0;
         //uint32_t ncols_dummy = 0;
@@ -58,7 +70,7 @@ struct CSC: public Compressed_Format<Weight> {
 
 
 template<typename Weight>
-CSC<Weight>::CSC(uint64_t nnz_, uint32_t nrows_, uint32_t ncols_) {
+CSC<Weight>::CSC(const uint64_t nnz_, const uint32_t nrows_, const uint32_t ncols_) {
     Compressed_Format<Weight>::compression_type = COMPRESSED_FORMAT::_CSC_;
     CSC::nnz = nnz_;
     CSC::nrows = nrows_; 
@@ -71,14 +83,13 @@ CSC<Weight>::CSC(uint64_t nnz_, uint32_t nrows_, uint32_t ncols_) {
 }
 
 template<typename Weight>
-void CSC<Weight>::populate(std::vector<struct Triple<Weight>>& triples, uint32_t tile_height, uint32_t tile_width) {
+void CSC<Weight>::populate(const std::vector<struct Triple<Weight>> triples, const uint32_t tile_height, const uint32_t tile_width) {
     uint32_t* IA = CSC::IA_blk->ptr;
     uint32_t* JA = CSC::JA_blk->ptr;
     Weight* A = CSC::A_blk->ptr;
     
     uint32_t i = 0;
     uint32_t j = 1; 
-    
     JA[0] = 0;
     for(auto &triple: triples) {
         std::pair pair = std::make_pair((triple.row % tile_height), (triple.col % tile_width));
@@ -97,77 +108,169 @@ void CSC<Weight>::populate(std::vector<struct Triple<Weight>>& triples, uint32_t
         JA[j] = JA[j - 1];
     }
     CSC::nnz_i = CSC::nnz;
-    
-    
-    //for(j = 0; j < CSC::ncols + 1; j++) {
-      //  printf("%d %d\n", j, JA[j]);
-    //}
-    
-    
- //for(j = 0; j < CSC::ncols + 1; j++) {
-   //     printf("%d %d\n", j, JA[j]);
-    //}
-    
-
-    //printf("XXXXXXXXXX %d %d \n", tile_width, CSC::ncols);
-    
-    for(int32_t t = Env::nthreads; t > 0; t--) {
-        uint32_t c1 = ((t - 1) * (CSC::ncols/Env::nthreads)) + 1;
-        uint32_t c2 =   t      * (CSC::ncols/Env::nthreads);
-        
-        for(j = c2; j >= c1; j--) {
-            JA[j] = JA[j - t];
-        }
-    }
-    
-    //printf("Walk this\n");
-   //CSC::walk(1);
-    
-    
-    //for(j = 0; j < CSC::ncols + 1; j++) {
-    //    printf("%d %d\n", j, JA[j]);
-    //}
-    
-    //std::exit(0);
-    
 }
 
 template<typename Weight>
-void CSC<Weight>::walk(int32_t tid) {  
+void CSC<Weight>::refine_cols() { 
+    uint32_t* IA = CSC::IA_blk->ptr;
+    uint32_t* JA = CSC::JA_blk->ptr;
+    Weight*    A = CSC::A_blk->ptr;
+    
+    uint32_t c1 = 0;
+    uint32_t c2 = 0;
+    for(int32_t t = Env::nthreads; t > 0; t--) {
+        c1 = ((t - 1) * (CSC::ncols/Env::nthreads)) + 1;
+        c2 =   t      * (CSC::ncols/Env::nthreads);
+        
+        for(uint32_t j = c2; j >= c1; j--) {
+            JA[j] = JA[j - t];
+        }
+    }
+}
+
+template<typename Weight>
+void CSC<Weight>::refine_rows(const uint32_t nrows) {
+    uint32_t* IA = CSC::IA_blk->ptr;
+    uint32_t* JA = CSC::JA_blk->ptr;
+    Weight*    A = CSC::A_blk->ptr;
+    
+    for(uint32_t j = 0; j < CSC::ncols; j++) {
+        for(uint32_t i = JA[j]; i < JA[j + 1]; i++) {
+            //printf("%d --> ", IA[i]);
+            IA[i] += (IA[i] / ((nrows - Env::nthreads) / Env::nthreads)) + 1;
+            //printf("%d\n", IA[i]);
+        }
+    }
+    //std::exit(0);
+}
+
+/*
+template<typename Weight>
+void populate(const std::vector<struct Triple<Weight>> triples, const uint32_t nrows, const uint32_t ncols, const uint32_t tile_height, const uint32_t tile_width) {
+    
+    
+    
+    //uint32_t l = B_IA[k];
+    //            l +=  1 + l/((1028 - Env::nthreads) / Env::nthreads);
+    
+};
+*/
+
+
+template<typename Weight>
+inline void CSC<Weight>::populate_spa(std::vector<Weight>& spa, const std::vector<Weight> bias, const uint32_t col, const int32_t tid) {
+    uint64_t&  k = Env::index_nnz[tid];
+    uint32_t   c = col + Env::start_col[tid] + 1;// + tid;
+    //uint32_t   c = col + Env::start_col[tid];
+    uint32_t* IA = CSC::IA_blk->ptr;
+    uint32_t* JA = CSC::JA_blk->ptr;
+    Weight*    A = CSC::A_blk->ptr;
+    
+    Weight YMIN = 0;
+    Weight YMAX = 32;
+
+    JA[c] = k;
+    for(uint32_t i = 0; i < CSC::nrows; i++) {
+        if(spa[i]) {
+            spa[i] += bias[c];
+            if(spa[i] < YMIN) {
+                spa[i] = YMIN;
+            }
+            else if(spa[i] > YMAX) {
+                spa[i] = YMAX;
+            }
+            if(spa[i]) {
+                JA[c]++;
+                IA[k] = i;
+                A[k] = spa[i];
+                k++;
+                spa[i] = 0;
+            }
+        }
+    }
+    
+    if(tid) {
+        if((c < Env::start_col[tid] + 10 ) or (c > Env::end_col[tid] - 10))
+         printf("%d %d %d %lu %d\n", col, c, JA[c], k, Env::start_col[tid]);
+        //if(c == 520) {
+        //if(c == 10) {
+          //  for(uint32_t ii = Env::start_col[tid]; ii < Env::start_col[tid]+10; ii++) 
+            //    printf(" %d %d\n", ii, JA[ii]);
+            
+            //std::exit(0);
+        //}
+    }
+}
+
+
+
+template<typename Weight>
+void CSC<Weight>::walk(const int32_t tid) {  
     uint32_t* IA = CSC::IA_blk->ptr;
     uint32_t* JA = CSC::JA_blk->ptr;
     Weight*    A = CSC::A_blk->ptr;
     
     
-    uint32_t start_col = Env::start_col[tid];
+    uint32_t start_col = Env::start_col[tid];// + tid + 1;
     uint32_t end_col = Env::end_col[tid];
-   // printf("1. %d %d %d %d\n", tid, start_col, end_col, end_col - start_col);
+    
+    printf("tid=%d start=%d/%d end=%d/%d len=%d/%d\n", tid, start_col, start_col - 1, end_col, end_col-1, end_col - start_col + 1, end_col - start_col + 1 -1 );
+    #pragma omp barrier
+    
+    /*
+    if(tid) {
+        int t = JA[start_col];
+        //JA[start_col] = JA[start_col-1];    
+        JA[start_col+1] = Env::offset_nnz[tid];
+
+        for(uint32_t j = 0; j < CSC::ncols; j++) {  
+            int d = (j < CSC::ncols/2) ? 0 : tid;
+            int jj = j - d;
+            if(j < 520)
+            std::cout << "j=" << j << ": jj=" << jj << "--" << jj+1 << ":" << JA[jj] << "--" << JA[jj + 1] << ": " <<  JA[jj + 1] - JA[jj] << std::endl;
+        }
+        printf("%d JA=%d\n", start_col, t);
+            printf("JA=%d\n", JA[start_col]);
+    }
+    */
+    
+   // std::exit(0);
     
     //start_col = (((CSC::ncols )/Env::nthreads) *  tid  ) + 1;
     //end_col   =  ((CSC::ncols )/Env::nthreads) * (tid+1);
     //start_col = 0; 
     //end_col = CSC::ncols;
     //printf("2.%d %d %d %d\n", tid, start_col, end_col, end_col - start_col);
-    
+    //end_col++;
     uint32_t displacement_nnz = Env::displacement_nnz[tid];
     //printf("%d %d %d\n", tid, start_col, end_col);
     Env::checksum[tid] = 0;
     Env::checkcount[tid] = 0;
-    if(tid){
-    for(uint32_t j = 0; j < end_col; j++) {  
-        uint32_t k = (j == start_col) ? displacement_nnz : 0;
+    /*
+    uint32_t displacement = (tid == 0) ? 0 : Env::offset_nnz[tid] - Env::index_nnz[tid-1];
+    Env::displacement_nnz[tid] = displacement;
+    JA[start_col] += Env::displacement_nnz[tid];
+    */ 
+ 
+    for(uint32_t j = start_col; j < end_col; j++) {  
+    //for(uint32_t j = 0; j < CSC::ncols; j++) {  
+       // uint32_t k = (j == start_col) ? displacement_nnz : 0;
        //uint32_t k = 0;
-       // if(tid)
-        std::cout << "j=" << j << ": " << JA[j] << "--" << JA[j + 1] << ": " <<  JA[j + 1] - JA[j] << std::endl;
-        for(uint32_t i = JA[j] + k; i < JA[j + 1]; i++) {
+       // if(tid and ((j < start_col + 10) or (j > end_col - 10)))
+           if(tid)
+               std::cout << "j=" << j-(tid+1) << ": " << JA[j] << "--" << JA[j + 1] << ": " <<  JA[j + 1] - JA[j] << std::endl;
+
+        
+        for(uint32_t i = JA[j]; i < JA[j + 1]; i++) {
             (void) IA[i];
             (void) A[i];
             Env::checksum[tid] += A[i];
             Env::checkcount[tid]++;
             //std::cout << "    i=" << i << ",i=" << IA[i] <<  ",value=" << A[i] << std::endl;
         }
-    }    
-    }
+    }   
+   
+    //}
 
     Env::barrier();
     #pragma omp barrier  
@@ -195,8 +298,52 @@ void CSC<Weight>::walk(int32_t tid) {
     #pragma omp barrier    
 }
 
+
 template<typename Weight>
-void CSC<Weight>::statistics(int32_t tid) {  
+void CSC<Weight>::walk(const bool one_rank) {  
+    uint32_t* IA = CSC::IA_blk->ptr;
+    uint32_t* JA = CSC::JA_blk->ptr;
+    Weight*    A = CSC::A_blk->ptr;
+    
+    double checksum = 0;
+    uint64_t checkcount = 0;
+    
+    for(uint32_t j = 0; j < CSC::ncols; j++) {  
+        //std::cout << "j=" << j << ": " << JA[j] << "--" << JA[j + 1] << ": " <<  JA[j + 1] - JA[j] << std::endl;
+        for(uint32_t i = JA[j]; i < JA[j + 1]; i++) {
+            (void) IA[i];
+            (void) A[i];
+            checksum += A[i];
+            checkcount++;
+        }
+    }
+    
+    Env::barrier();
+    if(one_rank) {
+        Logging::print(Logging::LOG_LEVEL::INFO, "Iteration=%d, Total checksum=%f, Total count=%d\n", Env::iteration, checksum, checkcount);
+    }
+    else {
+        uint64_t nnz_ = CSC::nnz;
+        uint64_t nnz_ranks = 0;
+        double sum_ranks = 0;
+        uint64_t count_ranks = 0;
+        MPI_Allreduce(&nnz_, &nnz_ranks, 1, MPI_UNSIGNED_LONG, MPI_SUM, MPI_COMM_WORLD);
+        MPI_Allreduce(&checksum, &sum_ranks, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+        MPI_Allreduce(&checkcount, &count_ranks, 1, MPI_UNSIGNED_LONG, MPI_SUM, MPI_COMM_WORLD);
+        
+        if(count_ranks != nnz_ranks) {
+            Logging::print(Logging::LOG_LEVEL::WARN, "Compression checksum warning!!\n");
+        }
+
+        Logging::print(Logging::LOG_LEVEL::INFO, "Iteration=%d, Total checksum=%f, Total count=%d\n", Env::iteration, sum_ranks, count_ranks);
+    }    
+}
+
+
+
+/*
+template<typename Weight>
+void CSC<Weight>::statistics(const int32_t tid) {  
     if(!tid) {
         
         //double nnz_ranks = 0;
@@ -205,41 +352,10 @@ void CSC<Weight>::statistics(int32_t tid) {
         Logging::print(Logging::LOG_LEVEL::INFO, "%lu %lu %f\n", CSC::nnz, CSC::nnz_i, (double) CSC::nnz_i/CSC::nnz);
     }
 }
+*/
 
 template<typename Weight>
-inline void CSC<Weight>::populate_spa(std::vector<Weight>& spa, std::vector<Weight> bias, const uint32_t col, int32_t tid) {
-    uint64_t&  k = Env::index_nnz[tid];
-    uint32_t   c = col + Env::start_col[tid] + 1;
-    uint32_t* IA = CSC::IA_blk->ptr;
-    uint32_t* JA = CSC::JA_blk->ptr;
-    Weight*    A = CSC::A_blk->ptr;
-    
-    Weight YMIN = 0;
-    Weight YMAX = 32;
-
-    JA[c] = k;
-    for(uint32_t i = 0; i < CSC::nrows; i++) {
-        if(spa[i]) {
-            spa[i] += bias[c];
-            if(spa[i] < YMIN) {
-                spa[i] = YMIN;
-            }
-            else if(spa[i] > YMAX) {
-                spa[i] = YMAX;
-            }
-            if(spa[i]) {
-                JA[c]++;
-                IA[k] = i;
-                A[k] = spa[i];
-                k++;
-                spa[i] = 0;
-            }
-        }
-    }
-}
-
-template<typename Weight>
-void CSC<Weight>::reallocate(uint64_t nnz_, uint32_t nrows_, uint32_t ncols_) {
+void CSC<Weight>::reallocate(const uint64_t nnz_, const uint32_t nrows_, const uint32_t ncols_) {
     double start_time = Env::tic();
     
     if(CSC::ncols != ncols_) {
@@ -262,7 +378,7 @@ void CSC<Weight>::reallocate(uint64_t nnz_, uint32_t nrows_, uint32_t ncols_) {
 }
 
 template<typename Weight>
-void CSC<Weight>::adjust(int32_t tid){
+void CSC<Weight>::adjust(const int32_t tid){
     uint32_t displacement = (tid == 0) ? 0 : Env::offset_nnz[tid] - Env::index_nnz[tid-1];
     Env::displacement_nnz[tid] = displacement;
     #pragma omp barrier
