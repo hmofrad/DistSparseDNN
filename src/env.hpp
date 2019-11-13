7a5c042d8e0c83bd6ef198c928f2437436d619f8
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-11-13 08:49:18 -0500

    Update src/env.hpp

--- src/env.hpp
+++ src/env.hpp
@@ -93,8 +93,9 @@ int Env::init() {
 }
 
 void Env::assign_col(uint32_t ncols, int32_t tid) {
-    Env::start_col[tid] = ((ncols/Env::nthreads) *  tid  ) + 1;
+    Env::start_col[tid] = ((ncols/Env::nthreads) *  tid  )+1;
     Env::end_col[tid]   =  (ncols/Env::nthreads) * (tid+1);
+    
     //printf("%d: %d: %d %d\n", tid, ncols, Env::start_col[tid], Env::end_col[tid]);
     //std::exit(0);
 }
