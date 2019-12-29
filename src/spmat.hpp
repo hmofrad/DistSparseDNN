d98becee797896ac65193a221c1b1e845c89edff
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-12-28 22:07:55 -0500

    Update src/spmat.hpp

--- src/spmat.hpp
+++ src/spmat.hpp
@@ -22,10 +22,11 @@ struct CSC {
         void populate(const std::vector<struct Triple<Weight>> triples, const uint32_t tile_height, const uint32_t tile_width);
         void populate(const std::vector<struct Triple<Weight>> triples, const uint32_t start_row, const uint32_t tile_height, const uint32_t start_col, const uint32_t tile_width);
         void populate_spa(Weight** spa, const Weight* bias, const uint32_t col,  uint64_t& index, const int32_t tid);
-        void walk(const bool one_rank);
-        void walk(const bool one_rank, const int32_t tid);
-        void reallocate(const uint64_t nnz_, const uint32_t nrows_, const uint32_t ncols_);
+        void walk_dxm(const bool one_rank, const int32_t leader_tid, const int32_t tid);
+        void walk_dxd(const bool one_rank, const int32_t leader_tid, const int32_t tid);
+        void reallocate(const uint64_t nnz_, const uint32_t nrows_, const uint32_t ncols_, const int32_t leader_tid, const int32_t tid);
         void adjust(const int32_t tid);
+        void adjust(const int32_t leader_tid, const int32_t tid);
         void repopulate(const std::shared_ptr<struct CSC<Weight>> other, const uint32_t start_col, const uint32_t end_col, const uint32_t dis_nnz, const int32_t tid);
         void repopulate(const std::shared_ptr<struct CSC<Weight>> other, const int32_t tid, const int32_t leader, const std::vector<int32_t> my_follower_threads);
 
@@ -149,33 +150,89 @@ void CSC<Weight>::populate_spa(Weight** spa, const Weight* bias, const uint32_t 
 }
 
 template<typename Weight>
-void CSC<Weight>::walk(const bool one_rank, const int32_t tid) {
+void CSC<Weight>::walk_dxm(const bool one_rank, const int32_t leader_tid, const int32_t tid) {  
+    if(tid == leader_tid) {
+        uint32_t* IA = CSC::IA_blk->ptr;
+        uint32_t* JA = CSC::JA_blk->ptr;
+        Weight*    A = CSC::A_blk->ptr;
+        
+        double checksum = 0;
+        uint64_t checkcount = 0;
+        for(uint32_t j = 0; j < CSC::ncols; j++) {
+            //if(!Env::rank)
+            //    std::cout << "j=" << j << "," << j << ": " << JA[j] << "--" << JA[j + 1] << ": " <<  JA[j + 1] - JA[j] << std::endl
+            for(uint32_t i = JA[j]; i < JA[j + 1]; i++) {
+                (void) IA[i];
+                (void) A[i];
+                checksum += A[i];
+                checkcount++;
+                //std::cout << "    i=" << i << ",i=" << IA[i] <<  ",value=" << A[i] << std::endl;
+            }
+        }
+
+        Env::barrier();
+        if(one_rank) {
+            Logging::print(Logging::LOG_LEVEL::INFO, "Iteration=%d, Total checksum=%f, Total count=%d\n", Env::iteration, checksum, checkcount);
+        }
+        else {
+            uint64_t nnz_ = CSC::nnz_i;
+            uint64_t nnz_ranks = 0;
+            double sum_ranks = 0;
+            uint64_t count_ranks = 0;
+            MPI_Allreduce(&nnz_, &nnz_ranks, 1, MPI_UNSIGNED_LONG, MPI_SUM, MPI_COMM_WORLD);
+            MPI_Allreduce(&checksum, &sum_ranks, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+            MPI_Allreduce(&checkcount, &count_ranks, 1, MPI_UNSIGNED_LONG, MPI_SUM, MPI_COMM_WORLD);
+
+            if(count_ranks != nnz_ranks) {
+                Logging::print(Logging::LOG_LEVEL::WARN, "Compression checksum warning!!\n");
+            }
+            Logging::print(Logging::LOG_LEVEL::INFO, "Iteration=%d, Total checksum=%f, Total count=%d\n", Env::iteration, sum_ranks, count_ranks);
+        } 
+    }    
+}
+
+template<typename Weight>
+void CSC<Weight>::walk_dxd(const bool one_rank, const int32_t leader_tid, const int32_t tid) {
     uint32_t* IA = CSC::IA_blk->ptr;
     uint32_t* JA = CSC::JA_blk->ptr;
     Weight*    A = CSC::A_blk->ptr;
     
-    Env::checksum[tid]   = 0;
-    Env::checkcount[tid] = 0;    
-    Env::checknnz[tid]   = CSC::nnz_i;
+    double&   checksum   = Env::counters[tid].checksum;
+    uint64_t& checkcount = Env::counters[tid].checkcount;
+    uint64_t& checknnz   = Env::counters[tid].checknnz;
+    
+    checksum   = 0;
+    checkcount = 0;    
+    checknnz   = CSC::nnz_i;
 
     for(uint32_t j = 0; j < CSC::ncols; j++) { 
-        //if(!Env::rank)
+        //if(!Env::rank and !tid)
         //    std::cout << "j=" << j << "," << j << ": " << JA[j] << "--" << JA[j + 1] << ": " <<  JA[j + 1] - JA[j] << std::endl;    
         for(uint32_t i = JA[j]; i < JA[j + 1]; i++) {
             (void) IA[i];
             (void) A[i];
-            Env::checksum[tid] += A[i];
-            Env::checkcount[tid]++;
+            checksum += A[i];
+            checkcount++;
+            //if(!Env::rank and !tid)
             //std::cout << "    i=" << i << ",i=" << IA[i] <<  ",value=" << A[i] << std::endl;
         }
     }   
     
     Env::barrier();
     pthread_barrier_wait(&Env::thread_barrier);
-    if(!tid) {
-        double     sum_threads = std::accumulate(Env::checksum.begin(),   Env::checksum.end(),   0);
-        uint64_t count_threads = std::accumulate(Env::checkcount.begin(), Env::checkcount.end(), 0);
-        uint64_t   nnz_threads = std::accumulate(Env::checknnz.begin(), Env::checknnz.end(), 0);
+    if(tid == leader_tid) {
+        double     sum_threads = 0;//std::accumulate(checksum.begin(),   checksum.end(),   0);
+        uint64_t count_threads = 0;//std::accumulate(checkcount.begin(), checkcount.end(), 0);
+        uint64_t   nnz_threads = 0;//std::accumulate(checknnz.begin(), checknnz.end(), 0);
+        
+        //std::vector<struct Env::thread_struct>::iterator it;
+        for(auto it = Env::counters.begin(); it != Env::counters.end(); it++) {
+            sum_threads   += (*it).checksum;
+            count_threads += (*it).checkcount;
+            nnz_threads   += (*it).checknnz;
+        }
+        
+        
         if(one_rank) {
             Logging::print(Logging::LOG_LEVEL::INFO, "Iteration=%d, Total checksum=%f, Total count=%d\n", Env::iteration, sum_threads, count_threads);
         }
@@ -187,7 +244,7 @@ void CSC<Weight>::walk(const bool one_rank, const int32_t tid) {
             MPI_Allreduce(&count_threads, &count_ranks, 1, MPI_UNSIGNED_LONG, MPI_SUM, MPI_COMM_WORLD);
             
             if(count_threads != nnz_threads) {
-                Logging::print(Logging::LOG_LEVEL::WARN, "Compression checksum warning!!\n");
+                Logging::print(Logging::LOG_LEVEL::WARN, "Compression checksum warning!! (%lu != %lu)\n", count_threads, nnz_threads);
             }
             Logging::print(Logging::LOG_LEVEL::INFO, "Iteration=%d, Total checksum=%f, Total count=%d\n", Env::iteration, sum_ranks, count_ranks);
         }
@@ -195,75 +252,47 @@ void CSC<Weight>::walk(const bool one_rank, const int32_t tid) {
 }
 
 template<typename Weight>
-void CSC<Weight>::walk(const bool one_rank) {  
-    uint32_t* IA = CSC::IA_blk->ptr;
-    uint32_t* JA = CSC::JA_blk->ptr;
-    Weight*    A = CSC::A_blk->ptr;
-    
-    double checksum = 0;
-    uint64_t checkcount = 0;
-    for(uint32_t j = 0; j < CSC::ncols; j++) {
-        //if(!Env::rank)
-        //    std::cout << "j=" << j << "," << j << ": " << JA[j] << "--" << JA[j + 1] << ": " <<  JA[j + 1] - JA[j] << std::endl
-        for(uint32_t i = JA[j]; i < JA[j + 1]; i++) {
-            (void) IA[i];
-            (void) A[i];
-            checksum += A[i];
-            checkcount++;
-            //std::cout << "    i=" << i << ",i=" << IA[i] <<  ",value=" << A[i] << std::endl;
-        }
-    }
-
-    Env::barrier();
-    if(one_rank) {
-        Logging::print(Logging::LOG_LEVEL::INFO, "Iteration=%d, Total checksum=%f, Total count=%d\n", Env::iteration, checksum, checkcount);
-    }
-    else {
-        uint64_t nnz_ = CSC::nnz_i;
-        uint64_t nnz_ranks = 0;
-        double sum_ranks = 0;
-        uint64_t count_ranks = 0;
-        MPI_Allreduce(&nnz_, &nnz_ranks, 1, MPI_UNSIGNED_LONG, MPI_SUM, MPI_COMM_WORLD);
-        MPI_Allreduce(&checksum, &sum_ranks, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
-        MPI_Allreduce(&checkcount, &count_ranks, 1, MPI_UNSIGNED_LONG, MPI_SUM, MPI_COMM_WORLD);
-
-        if(count_ranks != nnz_ranks) {
-            Logging::print(Logging::LOG_LEVEL::WARN, "Compression checksum warning!!\n");
-        }
-        Logging::print(Logging::LOG_LEVEL::INFO, "Iteration=%d, Total checksum=%f, Total count=%d\n", Env::iteration, sum_ranks, count_ranks);
-    }  
-}
-
-template<typename Weight>
-void CSC<Weight>::reallocate(const uint64_t nnz_, const uint32_t nrows_, const uint32_t ncols_) {
+void CSC<Weight>::reallocate(const uint64_t nnz_, const uint32_t nrows_, const uint32_t ncols_, const int32_t leader_tid, const int32_t tid) {
     if(CSC::ncols != ncols_) {
         Logging::print(Logging::LOG_LEVEL::ERROR, "Cannot reallocate.\n");
         std::exit(Env::finalize());     
     }
-    CSC::nnz = nnz_;
-    CSC::nnz_i = 0;
-    CSC::nrows = nrows_; 
-    CSC::ncols = ncols_;
-    
-    CSC::IA_blk->reallocate(CSC::nnz);
-    CSC::A_blk->reallocate(CSC::nnz);
+    if((leader_tid == -1) or (tid == leader_tid)) {
+        CSC::nnz = nnz_;
+        CSC::nnz_i = 0;
+        CSC::nrows = nrows_; 
+        CSC::ncols = ncols_;
         
-    CSC::JA_blk->clear();
-    CSC::IA_blk->clear();
-    CSC::A_blk->clear();
+        CSC::IA_blk->reallocate(CSC::nnz);
+        CSC::A_blk->reallocate(CSC::nnz);
+            
+        CSC::JA_blk->clear();
+        CSC::IA_blk->clear();
+        CSC::A_blk->clear();
+    }
 }
 
 template<typename Weight>
-void CSC<Weight>::adjust(const int32_t tid){    
-    uint32_t displacement = (tid == 0) ? 0 : Env::offset_nnz[tid] - Env::index_nnz[tid-1];
-    Env::displacement_nnz[tid] = displacement;
+void CSC<Weight>::adjust(const int32_t tid){
+    CSC::nnz_i = Env::threads[tid].idx_nnz;
+}
 
+template<typename Weight>
+void CSC<Weight>::adjust(const int32_t leader_tid, const int32_t tid){    
+    //uint32_t displacement = (tid == 0) ? 0 : Env::offset_nnz[tid] - Env::index_nnz[tid-1];
+    //Env::displacement_nnz[tid] = displacement;
+    Env::threads[tid].dis_nnz = (tid == 0) ? 0 : Env::threads[tid].off_nnz - Env::threads[tid-1].idx_nnz;
     pthread_barrier_wait(&Env::thread_barrier);
-    if(!tid) {
+    if((leader_tid == -1) or (tid == leader_tid)) {
+        //CSC::nnz_i = 0;
+        //for(int32_t i = 0; i < Env::nthreads; i++) {    
+        //    CSC::nnz_i += (Env::index_nnz[i] - Env::offset_nnz[i]);
+        //}
         CSC::nnz_i = 0;
-        for(int32_t i = 0; i < Env::nthreads; i++) {    
-            CSC::nnz_i += (Env::index_nnz[i] - Env::offset_nnz[i]);
-        }
+        //std::vector<struct Env::thread_struct>::iterator it;
+        for(auto it = Env::threads.begin(); it != Env::threads.end(); it++) {
+            CSC::nnz_i += ((*it).idx_nnz - (*it).off_nnz);
+        }        
     }
     pthread_barrier_wait(&Env::thread_barrier);
 }
