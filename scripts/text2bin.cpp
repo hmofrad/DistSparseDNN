bf3485e26caa29c3982251be9f3dde208bb1705f
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2020-05-03 11:42:21 -0400

    Update text2bin.cpp

--- scripts/text2bin.cpp
+++ scripts/text2bin.cpp
@@ -12,6 +12,7 @@
 /* mode: 1 = Single column input text file
          2 = Two columns input text file
          3 = Three columns input text file (3rd column as double/float weights)
+		 4 = Two columns input text file
  */
  
 using WGT = float; 
@@ -29,21 +30,21 @@ int main(int argc, char **argv) {
     
     std::ifstream fin(filepath_in.c_str(),   std::ios_base::in);
     if(!fin.is_open()) {
-        fprintf(stderr, "Unable to open input file %s.\n", filepath_in.c_str());
+        fprintf(stderr, "Unable to open input file %s\n", filepath_in.c_str());
         std::exit(1); 
     }
     
     std::ofstream fout(filepath_out.c_str(), std::ios_base::out);
     if(!fout.is_open()) {
-        fprintf(stderr, "Unable to open output file %s.\n", filepath_out.c_str());
+        fprintf(stderr, "Unable to open output file %s\n", filepath_out.c_str());
         std::exit(1); 
     }
     
     uint64_t num_edges = 0;
     uint32_t num_rows = 0;
     uint32_t num_cols = 0;
-    uint32_t i, j;
-    WGT w;
+    uint32_t i = 0, j = 0;
+    WGT w = .0;
     std::string line;
     std::istringstream iss;
     while (std::getline(fin, line)) {
@@ -68,16 +69,21 @@ int main(int argc, char **argv) {
             fout.write(reinterpret_cast<const char*>(&w), sizeof(WGT));
             //std::cout << "i=" << i << " j=" << j << " w=" << w << std::endl;
         }
+		else if(mode == 4) {
+            iss >> i >> w;
+            fout.write(reinterpret_cast<const char*>(&i), sizeof(uint32_t));
+            fout.write(reinterpret_cast<const char*>(&j), sizeof(WGT));
+            //std::cout << "i=" << i << " j=" << j << std::endl;
+        }
         
         num_edges++;
         num_rows = (num_rows < i) ? i : num_rows;
         num_cols = (num_cols < j) ? j : num_cols;
-        
     }
     fout.close();
     fin.close();
 	
-    std::cout << "File \"" << filepath_in << "\": [" << num_rows << " x " << num_cols << "]" << ", nnz=" <<  num_edges << " convertd into" << std::endl;
+    std::cout << "File \"" << filepath_in << "\": [" << num_rows+1 << " x " << num_cols+1 << "]" << ", nnz=" <<  num_edges << " convertd into" << std::endl;
     std::cout << "File \"" << filepath_out << "\"." << std::endl;
 
 	return(0);
