9098bc91c50f61a0c51a1d2e09f0da082bee9cf9
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2020-07-08 22:11:16 -0400

    Update src/apps/mnist.cpp

--- src/apps/mnist.cpp
+++ src/apps/mnist.cpp
@@ -1,5 +1,5 @@
 /*
- * sparse_mnist.cpp: Sparse DNN inference for MNIST dataset
+ * mnist.cpp: Sparse DNN inference for MNIST dataset
  * [http://yann.lecun.com/exdb/mnist/]
  * (c) Mohammad Hasanzadeh Mofrad, 2020
  * (e) m.hasanzadeh.mofrad@gmail.com
@@ -26,6 +26,7 @@
 using WGT = float;
 WGT noop(WGT w) {return w;}
 WGT relu(WGT w) {return (w < 0) ? 0 : w;}
+const std::string classifier = "softmax";
 
 int main(int argc, char **argv) {
     Logging::enabled = true;
@@ -106,7 +107,7 @@ int main(int argc, char **argv) {
 			   nneurons, nmax_layers, layer_files, 
 			   bias_value, bias_type, bias_files, 
 			   ncategories, category_type, category_file, 
-			   noop, relu, "softmax",
+			   noop, relu, classifier,
 			   input_type, parallelism_type, compression_type, hashing_type);
     
     return(Env::finalize());
