190a93bb46a18874da71eb165e312f7d1c66fdde
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2020-07-17 19:41:45 -0400

    Update src/apps/radixnet.cpp

--- src/apps/radixnet.cpp
+++ src/apps/radixnet.cpp
@@ -5,7 +5,7 @@
  * (e) m.hasanzadeh.mofrad@gmail.com
  */
  
-// make clean && make && time mpirun.mpich -np 4 bin/./radixnet -m 60000 1024 -n 1024 -l 120 -c 0 data/radixnet/bin/MNIST data/radixnet/bin/DNN -p 0
+// make clean && make && time mpirun.mpich -np 4 bin/./radixnet -i 1000 1024 0 data/radixnet/bin/MNIST -n 1024 120 data/radixnet/bin/DNN -c 1 -m 0 -p 1 -h 1
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -27,85 +27,66 @@ using WGT = float;
 WGT noop(WGT w) {return w;}
 WGT relu(WGT w) {return (w < 0) ? 0 : (w > 32) ? 32 : w;}
 const std::string classifier = "softmax";
+std::vector<uint32_t> nneurons_vector = {1024, 4096, 16384, 65536};
+std::vector<uint32_t> nmax_layers_vector = {120, 480, 1920};
+std::vector<WGT> bias_vector = {-0.3,-0.35,-0.4,-0.45};
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
     Logging::enabled = true;
     int status = Env::init();
     if(status) {
         Logging::print(Logging::LOG_LEVEL::FATAL, "Failure to initialize MPI environment\n");
         std::exit(Env::finalize());   
     }
+    uint32_t input_ninstances, input_nfeatures, ncategories;
+    uint32_t nneurons, nmax_layers;
+    std::string input_path, layers_path;
+    uint32_t c, m, p, h;
+    Env::read_options(argc, argv, "MNIST", "Radix-Net", 
+                      input_ninstances, input_nfeatures, ncategories, input_path, 
+                      nneurons, nmax_layers, layers_path, 
+                      c, m, p, h);
+    FILE_TYPE file_type = FILE_TYPE::_BINARY_;
 
-    if(argc != 14) {
-        Logging::print(Logging::LOG_LEVEL::ERROR, "USAGE = %s -m <input_ninstances input_nfeatures> -n <nneurons> -l <nmax_layers> -c <ncategories> <path_to_input> <path_to_dnn> -p <parallelism_type>\n", argv[0]);
-        std::exit(Env::finalize());     
-    }
-    
-    std::string script = "radixnet";
-    Logging::print(Logging::LOG_LEVEL::INFO, "%s sparse DNN for MNIST dataset\n", script.c_str());
-    Logging::print(Logging::LOG_LEVEL::INFO, "Radix-Net sparse DNN for MNIST dataset Implementation\n");
-    Logging::print(Logging::LOG_LEVEL::INFO, "Machines = %d, MPI ranks  = %d, Threads per rank = %d\n", Env::nmachines, Env::nranks, Env::nthreads);
-    Logging::print(Logging::LOG_LEVEL::INFO, "Sockets  = %d, Processors = %d\n", Env::nsockets, Env::ncores);
-    if(Env::NUMA_ALLOC) Logging::print(Logging::LOG_LEVEL::INFO, "NUMA is enabled.\n", Env::NUMA_ALLOC);
-    else Logging::print(Logging::LOG_LEVEL::WARN, "NUMA is disabled.\n", Env::NUMA_ALLOC);
-    
-    uint32_t input_ninstances = atoi(argv[2]);
-    uint32_t input_nfeatures = atoi(argv[3]);
-    uint32_t nneurons = atoi(argv[5]);
-    uint32_t nmax_layers = atoi(argv[7]);
-    uint32_t ncategories = atoi(argv[9]);
-    std::string feature_file_prefix = ((std::string) argv[10]);
-    std::string layer_file_prefix = ((std::string) argv[11]);
-    INPUT_TYPE input_type = INPUT_TYPE::_BINARY_;
-    
-    std::vector<uint32_t> nneurons_vector = {1024, 4096, 16384, 65536};    
-    uint32_t idxN = std::distance(nneurons_vector.begin(), std::find(nneurons_vector.begin(), nneurons_vector.end(), nneurons));
-    if(idxN >= nneurons_vector.size()) {
-        Logging::print(Logging::LOG_LEVEL::ERROR, "Invalid number of neurons %d\n", nneurons);
-        std::exit(Env::finalize());
-    }    
-    
-    std::string feature_file = feature_file_prefix + "/sparse-images-" + std::to_string(nneurons);
-    feature_file += (input_type == INPUT_TYPE::_TEXT_) ? ".tsv" : ".bin";
-    
-    std::vector<uint32_t> nmax_layers_vector = {120, 480, 1920};
-    uint32_t idxL = std::distance(nmax_layers_vector.begin(), std::find(nmax_layers_vector.begin(), nmax_layers_vector.end(), nmax_layers));
-    if(idxL >= nmax_layers_vector.size()) {
-        Logging::print(Logging::LOG_LEVEL::ERROR, "Invalid number of layers %d\n", nmax_layers);
-        std::exit(Env::finalize());
-    }
-    std::string category_file = layer_file_prefix + "/neuron" + std::to_string(nneurons) + "-l" + std::to_string(nmax_layers);
-    category_file += (input_type == INPUT_TYPE::_TEXT_) ? "-categories.tsv" : "-categories.bin";
+    std::string input_file = input_path + "/sparse-images-" + std::to_string(nneurons);
+    input_file += (file_type == FILE_TYPE::_TEXT_) ? ".tsv" : ".bin";
+
+    std::string category_file = layers_path + "/neuron" + std::to_string(nneurons) + "-l" + std::to_string(nmax_layers);
+    category_file += (file_type == FILE_TYPE::_TEXT_) ? "-categories.tsv" : "-categories.bin";
     VALUE_TYPE category_type = VALUE_TYPE::_NONZERO_INSTANCES_ONLY_;
     
     std::vector<std::string> layer_files;
     for(uint32_t i = 0; i < nmax_layers; i++) {
-        std::string layer_file = layer_file_prefix + "/neuron" + std::to_string(nneurons) + "/n" + std::to_string(nneurons) + "-l" + std::to_string(i+1);
-        layer_file += (input_type == INPUT_TYPE::_TEXT_) ? ".tsv" : ".bin";
+        std::string layer_file = layers_path + "/neuron" + std::to_string(nneurons) + "/n" + std::to_string(nneurons) + "-l" + std::to_string(i+1);
+        layer_file += (file_type == FILE_TYPE::_TEXT_) ? ".tsv" : ".bin";
         layer_files.push_back(layer_file);
     }
     
     std::vector<std::string> bias_files;
-    std::vector<WGT> bias_vector = {-0.3,-0.35,-0.4,-0.45};
-    WGT bias_value = bias_vector[idxN];
+    uint32_t index = std::distance(nneurons_vector.begin(), std::find(nneurons_vector.begin(), nneurons_vector.end(), nneurons));
+    WGT bias_value = bias_vector[index];
     VALUE_TYPE bias_type = VALUE_TYPE::_CONSTANT_;
     
-    int x = atoi(argv[13]);
-    PARALLELISM_TYPE parallelism_type = (PARALLELISM_TYPE)x;
-    if(parallelism_type >= (PARALLELISM_TYPE::_SIZE_)) {
-        Logging::print(Logging::LOG_LEVEL::FATAL, "Incorrect parallelism type\n");
+    COMPRESSED_FORMAT compression_type = (COMPRESSED_FORMAT)c;
+    MULTIPLICATION_TYPE multiplication_type = (MULTIPLICATION_TYPE)m;
+    PARALLELISM_TYPE parallelism_type = (PARALLELISM_TYPE)p;
+    HASHING_TYPE hashing_type = (HASHING_TYPE)h;
+    
+    if((compression_type >= (COMPRESSED_FORMAT::_C_SIZE_)) or 
+    (multiplication_type >= (MULTIPLICATION_TYPE::_M_SIZE_)) or
+    (hashing_type >= (HASHING_TYPE::_H_SIZE_)) or
+    (parallelism_type >= (PARALLELISM_TYPE::_P_SIZE_))) {
+        Logging::print(Logging::LOG_LEVEL::FATAL, "Incorrect parallelism,compression, or hashing type\n");
         std::exit(Env::finalize());
     }
-    
-    COMPRESSED_FORMAT compression_type = COMPRESSED_FORMAT::_CSC_;
-    HASHING_TYPE hashing_type = HASHING_TYPE::_BOTH_;
-    
-    Net<WGT> N(input_ninstances, input_nfeatures, feature_file,
+
+    Net<WGT> N(input_ninstances, input_nfeatures, input_file,
+               ncategories, category_type, category_file, 
                nneurons, nmax_layers, layer_files, 
                bias_value, bias_type, bias_files,
-               ncategories, category_type, category_file, 
                noop, relu, classifier,
-               input_type, parallelism_type, compression_type, hashing_type);
+               file_type, 
+               compression_type, multiplication_type, parallelism_type, hashing_type);
     
     return(Env::finalize());
 }
